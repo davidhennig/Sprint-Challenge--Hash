b1ef07b002c216b17fab0ea1af1701899db89d23
davidhennig <davidjhennig@gmail.com>
2020-05-08 14:24:38 -0400

    finished 2

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -10,18 +10,16 @@ def has_negatives(a):
 
     for num in a:
 
-        if abs(num) in dic:
-            if num > 0 and dic[abs(num)] == 'negative':
-                result.append(abs(num))
-            elif num < 0 and dic[abs(num)] == 'positive':
-                result.append(abs(num))
-
-
-        else:
+        if abs(num) not in dic:
             if num > 0:
                 dic[abs(num)] = 'positive'
             else:
                 dic[abs(num)] = 'negative'
+        else:
+            if num > 0 and dic[abs(num)] == 'negative':
+                result.append(abs(num))
+            elif num < 0 and dic[abs(num)] == 'positive':
+                result.append(abs(num))
 
 
 
